e95da738c3b6a048ebd4d3556c4f26184fdcf5b2
sohcrana <ovurucuel@gmail.com>
2019-05-31 18:05:38 +0300

    registration

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = require('../models/User');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 router.post('/', (req, res, next)=>{
-  
+  const {username, password} = req.body;
+  const user = new User ({
+    username,
+    password
+  });
+   const promise = user.save();
+   promise.then((data)=>{
+     res.json(data);
+   }).catch((err)=>{
+     res.json(err);
+   });
 });
 module.exports = router;
